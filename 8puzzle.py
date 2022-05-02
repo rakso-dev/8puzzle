b5d74e39efe5994128f8f90d712d2d829a5ad204
rakso-dev <oskarr.magana@gmail.com>
2022-05-02 11:18:23 -0500

    making skeleton

---
+++ 8puzzle.py
@@ -0,0 +1,22 @@
+class TreeNode:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.branches = []
+
+    def get_position():
+        pass
+    
+    def move_up():
+        pass
+
+    def move_right():
+        pass
+
+    def move_down():
+        pass
+
+    def move_left():
+        pass
+
+
+
